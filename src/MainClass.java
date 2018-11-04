bcc1e7ad76684c13962aa07bd74bfaa66bef3409
mparidabit4@gmail.com <mparidabit4@gmail.com>
2018-11-04 11:14:55 +0530

    Demo Java Git Commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,5 +3,6 @@ public class MainClass {
     public static void main(String args[]){
         System.out.println("In IntelliJ");
         System.out.println("In IntelliJ MSG 2");
+        System.out.println("In IntelliJ MSG 3");
     }
 }
